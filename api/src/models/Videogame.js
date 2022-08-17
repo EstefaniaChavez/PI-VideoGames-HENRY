1d0f8dc3f335e26849e5732e58d2f337bb25f31f
EstefaniaChavez <estefaniachavez1506@gmail.com>
2022-08-16 22:04:08 -0300

    Módelos terminados

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false, 
+      primaryKey:true, 
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released:{
+      type: DataTypes.STRING,
+      allowNull: true, 
+    },
+    rating:{
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    plataforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    background_image:{
+      type: DataTypes.STRING,
+      allowNull: true, 
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false, 
+      defaultValue: true, 
+    }
   });
 };
